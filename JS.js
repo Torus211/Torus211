5f088ea861324abaf8c1bc3243cb2adcc77470fc
Torus211 <torushidenami@gmail.com>
2024-10-22 21:17:40 +0300

    Add files via upload

--- JS.js
+++ JS.js
@@ -1,22 +1,25 @@
 
- function click1() {
- let f1 = document.getElementsByName("field1");
-  let quantity = parseInt(f1[0].value); 
- let r = document.getElementById("result");
- let s = document.getElementsByName("select1");
+function click1() {
+   let f1 = document.getElementsByName("field1");
+   let quantity = parseInt(f1[0].value);
+   let r = document.getElementById("result");
+   let s = document.getElementsByName("select1");
+   if (quantity > 0) {
+      if (s[0].value === "v1") {
+         r.innerHTML = quantity * 10 + " руб.";
+      }
+      else if (s[0].value === "v2") {
+         r.innerHTML = quantity * 15 + " руб.";
+      }
+   }
+   else errorMessage.textContent = "Пожалуйста, введите корректное количество больше нуля.";
+   return false;
+}
 
 
-    if (s[0].value === "v1") { 
-    r.innerHTML = quantity * 10 + " руб."; 
-    } 
-    else if (s[0].value === "v2") { 
- r.innerHTML = quantity * 15 + " руб."; 
- }
 
-return false; 
- }
 
- 
-  
- 
+
+
+
 
